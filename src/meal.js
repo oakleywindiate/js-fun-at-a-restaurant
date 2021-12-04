7d175cd58de9d62b13dbc71176d474f942696593
Oakley Windiate <oakley.windiate@gmail.com>
2021-12-04 14:52:04 -0700

    Add meal.js section

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,48 @@
-function nameMenuItem() {
+function nameMenuItem(item1) {
+ return "Delicious " + item1;
+};
 
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type,
+  };
+  return menuItem;
+};
+
+function addIngredients(foodItem, ingredients) {
+  if (ingredients.includes(foodItem)) {
+    return
+  //do nothing
+  } else {
+    ingredients.push(foodItem);
+ }
+return ingredients;
+};
+
+function formatPrice(initialPrice) {
+  return "$" + initialPrice;
+};
+
+function decreasePrice(decreasedPrice) {
+return (decreasedPrice - ((10 / 100) * decreasedPrice));
+};
+
+function createRecipe(title, ingredients, type) {
+var menuItem = {
+  title,
+  ingredients,
+  type,
 }
-
+return menuItem;
+};
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
