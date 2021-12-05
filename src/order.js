9e24a4ab0b235e5d70fda59ca919677127760f87
Oakley Windiate <oakley.windiate@gmail.com>
2021-12-04 19:22:00 -0700

    Add order.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,40 @@
 
+function takeOrder(order, orderArray) {
+  if (orderArray.length < 3) {
+    orderArray.push(order);
+  }
+  return orderArray;
+}
+
+function refundOrder(orderNumber, orderArray) {
+  for (var i = 0; i < orderArray.length; i++) {
+    if (orderArray[i].orderNumber === orderNumber) {
+      orderArray.splice(i, 1)
+    }
+  }
+}
+
+function listItems(orderArray) {
+var stringItems = [];
+  for (var i = 0; i < orderArray.length; i++) {
+    stringItems.push(orderArray[i].item);
+  }
+  return stringItems.join(", ")
+}
+
+function searchOrder(orderArray, orderItem) {
+var isOrdered = false;
+  for (var i = 0; i < orderArray.length; i++) {
+    if (orderArray[i].item === orderItem) {
+      isOrdered = true;
+    }
+  }
+  return isOrdered
+}
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
